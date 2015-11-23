7c3b377a3595c2f468f88e72d42db89a2559eaae
lokimart <msloekito@gmail.com>
2015-11-23 18:11:22 +0000

    added tdd

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,7 +2,7 @@ class QuotesController < ApplicationController
     def index
         @quote = Quote.order("RANDOM()").first
     end
-    
+
     def create
         @quote = Quote.create(quote_params)
         if @quote.invalid?
@@ -10,12 +10,19 @@ class QuotesController < ApplicationController
         end
         redirect_to root_path
     end
-    
+
     def about
     end
-    
+
+    def show
+        @quote = Quote.where(:id => params[:id]).first
+        if @quote.blank?
+            render :text => "Not Found", :status => :not_found
+        end
+    end
+
     private
-    
+
     def quote_params
         params.require(:quote).permit(:saying, :author)
     end
